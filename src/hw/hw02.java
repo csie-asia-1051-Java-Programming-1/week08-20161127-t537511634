6ea0a3cc916573674cce55e9efc05a6fc4a4bd54
t537511634 <105021029@live.asia.edu.tw>
2016-11-28 15:56:41 +0800

    OP hw2

--- src/hw/hw02.java
+++ src/hw/hw02.java
@@ -12,17 +12,22 @@ public class hw02 {
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
 		Scanner scn = new Scanner(System.in);
-		int v = scn.nextInt(),nu = scn.nextInt(),to = scn.nextInt();
-		System.out.println(t2(v,nu,to));
+		int v = scn.nextInt(),nu = scn.nextInt(),to = scn.nextInt(),tr=0,c=0;
+		System.out.println(t2(t10(v,nu,tr,c),to));
+		System.out.println(t10(v,nu,tr,c));
 
 	}
 
-	public static int t2(int v,int nu,int to) {
+	public static int t2(int v,int to) {
 		if(v>1){
-			return t2(v/nu,nu,to)*10+v%nu;
+			return t2(v/to,to)*10+v%to;
 		}else{
-			return v%nu;
+			return v%to;
 		}
+	}	
+	public static int t10(int v,int nu,int tr,int c) {
+		tr = (int) (v%10*Math.pow(nu,c));
+			return (v==0)?tr:tr+t10(v/10,nu,tr,c+=1);
+	}
 	}
 
-}
